6feb7a6cf0549e47f0d436b19ee392d56b042302
anggitprayogo <anggitprayogo@gmail.com>
2020-03-08 13:00:14 +0700

    initial commit

---
+++ part-2-next-gen-js/spread_and_rest_ops.js
@@ -0,0 +1,12 @@
+/**
+ * Spread => to split the array elements OR object properties
+ * ex: 
+ * const newArray = [...oldArray, 1, 2]
+ * const newObject = {...oldObject, newProp:5}
+ */
+
+
+
+ /**
+  * Rest => to merge a list arguments into array
+  */
---
+++ part-2-next-gen-js/variable.js
@@ -0,0 +1,11 @@
+/**
+ * var => it can fill with value, and we can change the value of the variabel
+ * let => same like var
+ * const => it can store value, but we can't change the variable value
+ */
+
+var name = "Anggit Prayogo"
+let name2 = "Anggit"
+name2 = "Prayogo"
+const name3 = "Anggit"
+console.log(name3)
