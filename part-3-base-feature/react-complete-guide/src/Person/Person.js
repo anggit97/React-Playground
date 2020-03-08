36a23b4b43320a42ad2f8f1ab67cf364ee64c8bb
anggitprayogo <anggitprayogo@gmail.com>
2020-03-09 06:55:29 +0700

    add external css file

--- part-3-base-feature/react-complete-guide/src/Person/Person.js
+++ part-3-base-feature/react-complete-guide/src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import './Person.css'
 
 const Person = (props) => {
-    return <div>
+    return <div className="Person">
         <p onClick={props.onClick}>Hello I'm {props.name} and I'm {props.age} year old</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
