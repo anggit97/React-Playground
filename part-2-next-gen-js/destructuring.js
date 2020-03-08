1a39f55b40d6fd4efd1c2e5bc41a4e58de0440f6
anggitprayogo <anggitprayogo@gmail.com>
2020-03-08 13:18:42 +0700

    add example of destructuring

---
+++ part-2-next-gen-js/destructuring.js
@@ -0,0 +1,15 @@
+/**
+ * Destructuring => Easily extract array elements or object properties and store them in variable
+ */
+//Array destructuring
+[a, b] = ["Hello", "Anggit Prayogo"]
+console.log(a)
+console.log(b)
+//output 
+//Hello
+//Anggit
+
+//Object destructuring
+const {name} = {name : "Anggit Prayogo"}
+console.log(name)
+//output : Anggit Prayogo
