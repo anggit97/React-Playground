fad5d3a25df5d5bb3edfdb89325f089e1b86afd7
anggitprayogo <anggitprayogo@gmail.com>
2020-03-08 14:51:08 +0700

    Add build_workflow.js

---
+++ part-3-base-feature/build_workflow.js
@@ -0,0 +1,13 @@
+/**
+ * Using Build Workflow
+ * Why? 
+ * 1. Optimize Code
+ * 2. Use Next-Gen Javascript Feature
+ * 3. Be More Productive
+ * 
+ * How?
+ * 1. Use Depedency Management Tool, ex: yarn or npm or etc
+ * 2. Use Bundler, ex: webpack : we want to make our code to be module, we bundle all things togather
+ * 3. Use Compiler, ex: Babel + Presents
+ * 4. Use Development Server
+ */
