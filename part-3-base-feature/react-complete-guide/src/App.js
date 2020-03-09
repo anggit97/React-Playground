87cfb48c8fd729e654ed3fc138cda85df0578da8
anggitprayogo <anggitprayogo@gmail.com>
2020-03-09 07:00:11 +0700

    add inline style

--- part-3-base-feature/react-complete-guide/src/App.js
+++ part-3-base-feature/react-complete-guide/src/App.js
@@ -37,10 +37,18 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <p>Hello World</p>
-        <button onClick={this.switchNameHandler.bind(this, "Kafir")}>Switch name click</button>
+        <button style={style} onClick={this.switchNameHandler.bind(this, "Kafir")}>Switch name click</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
