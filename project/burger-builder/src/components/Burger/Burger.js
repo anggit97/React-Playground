1eb6ca3acef130b3f8db1c8ecd5c1b98457f86c7
anggitprayogo <anggitprayogo@gmail.com>
2020-03-22 18:09:16 +0700

    Add transformedIngredients in Burger Component

--- project/burger-builder/src/components/Burger/Burger.js
+++ project/burger-builder/src/components/Burger/Burger.js
@@ -4,14 +4,23 @@ import styles from './Burger.module.css'
 import BurgerIngredinents from './BurgerIngredient/BurgerIngredient'
 
 const Burger = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map( igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredinents key={igKey+i} type={igKey}/>;
+        });
+    });
+
+    console.log(transformedIngredients)
+
     return(
         <div className={styles.Burger}>
             <BurgerIngredinents type="bread-top"/>
-            <BurgerIngredinents type="cheese"/>
-            <BurgerIngredinents type="meat"/>
+            {transformedIngredients}
             <BurgerIngredinents type="bread-bottom"/>
         </div>
     )
-}
+};
 
 export default Burger
